72770162e8727f67b0a6134adbffb1275c0e2165
Kunal Ananda Sagar <kunalsagar0615@gmail.com>
2026-03-21 15:27:48 +0530

    live link added of frontend

--- KeyStone_Banking_Application/src/main/java/com/example/demo/config/SercurityConfig.java
+++ KeyStone_Banking_Application/src/main/java/com/example/demo/config/SercurityConfig.java
@@ -51,7 +51,9 @@ public class SercurityConfig {
 
 	    CorsConfiguration config = new CorsConfiguration();
 
-	    config.setAllowedOrigins(List.of("http://localhost:5173"));
+	    config.setAllowedOrigins(List.of("http://localhost:5173",
+	            "https://keystone-banking-services.netlify.app")
+	    		);
 	    config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
 	    config.setAllowedHeaders(List.of("*"));
 	    config.setAllowCredentials(true);
